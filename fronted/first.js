bc83d742e24e8f37c7896c475e4149c39f9f4c42
yashaswinigande <gandeyashaswini431@gmail.com>
2025-09-13 10:35:12 +0530

    Initial commit

---
+++ fronted/first.js
@@ -0,0 +1,14 @@
+console.log("srit"
+var name1="srit";
+console.log(name1);
+var name1="hi";
+console.log(name1);
+let course="cse";
+let age=20;
+console.log(age);
+age=21;
+console.log(age);
+const givenname="srit";
+console.log(givenname);
+givenname="rit";
+console.log(givenname);
